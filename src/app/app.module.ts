f8f91a6686d4e0a7a12f185ab01c5a6e12ddc663
Vinay Dangi <vinaydangi145@gmail.com>
2023-10-03 14:40:09 +0530

    Component read

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MapComponent } from './components/map/map.component';
 import { AddressMainComponent } from './components/address-main/address-main.component';
+import { UserCurrentLocationComponent } from './components/user-current-location/user-current-location.component';
+import { TestMarkerComponent } from './components/test-marker/test-marker.component';
+// import { AgmCoreModule } from '@agm/core';  
+import { FormsModule } from '@angular/forms';
+import { HelloComponent } from './hello.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     MapComponent,
-    AddressMainComponent
+    AddressMainComponent,
+    UserCurrentLocationComponent,
+    HelloComponent,
+    TestMarkerComponent
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule,
+    FormsModule,
+    // AgmCoreModule.forRoot({
+    //   apiKey: 'AIzaSyDtnAASFjTTT2ufOxKKlD_RlrJ7axBleT0',
+    // }),  
+    
+  ], 
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent] 
 })
-export class AppModule { }
+export class AppModule { } 
+
+ 
+// npm install @agm/core@latest 
---
+++ src/app/components/user-current-location/user-current-location.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-current-location',
+  templateUrl: './user-current-location.component.html',
+  styleUrls: ['./user-current-location.component.css']
+})
+export class UserCurrentLocationComponent {
+  // name = 'Angular';
+  // public lat; 
+  // public lng; 
+
+  // public ngOnInit(): void {  
+  //   this.getLocation();
+  // } 
+ 
+  // getLocation() {
+  //   if (navigator.geolocation) {
+  //     navigator.geolocation.getCurrentPosition((position: Position) => {
+  //       if (position) {
+  //         console.log("Latitude: " + position.coords.latitude +
+  //           "Longitude: " + position.coords.longitude);
+  //         this.lat = position.coords.latitude;
+  //         this.lng = position.coords.longitude;
+  //         console.log(this.lat);
+  //         console.log(this.lat); 
+  //       }
+  //     },
+  //       (error: Position) => console.log(error));
+  //   } else {
+  //     alert("Geolocation is not supported by this browser.");
+  //   }
+  // } 
+}
+ 
+
