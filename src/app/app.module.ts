3bb89ef831282be2862b00e02129a15d02cdd473
amandangi23 <amandangi2723@gmail.com>
2023-10-03 15:28:24 +0530

    changes updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,25 @@ import { MapComponent } from './components/map/map.component';
 import { AddressMainComponent } from './components/address-main/address-main.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { CordinatesLatLongComponent } from './components/cordinates-lat-long/cordinates-lat-long.component';
+import { AgmCoreModule } from '@agm/core';
 
 @NgModule({
   declarations: [
     AppComponent,
     MapComponent,
-    AddressMainComponent
+    AddressMainComponent,
+    CordinatesLatLongComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
+    AgmCoreModule.forRoot({
+      // please get your own API key here:
+      // https://developers.google.com/maps/documentation/javascript/get-api-key?hl=en
+      apiKey: 'AIzaSyDtnAASFjTTT2ufOxKKlD_RlrJ7axBleT0'
+    })
     
   ],
   providers: [],
